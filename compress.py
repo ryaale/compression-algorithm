5b1eb77156599ebfcaa50b3664330e1eea8d3260
Alexander J. Ryan <alexanderjamesryan@gmail.com>
2018-05-05 11:57:40 +0200

    Add files via upload

--- compress.py
+++ compress.py
@@ -1,3 +1,18 @@
+'''
+Compress
+
+Authors: Sarah O'Connor
+         Alexander Ryan
+
+This program compresses a text file. The input is a text file
+and the ouput is a compressed version of the text file.
+
+Revision history:
+
+30 Sep 2014: Added comments
+
+'''
+
 VERSION = 1.0
 
 NUM_BYTES = 256
@@ -7,10 +22,51 @@ for byte_val in range(NUM_BYTES):
     ALL_BYTES.add(chr(byte_val))
 
 def get_unused_bytes(string):
+    '''
+    Returns the number of unique bytes 
+    that are not being used in the string.
+    
+    This gives us an indication of how many bytes 
+    we can use to substitute n_grams in the string.
+    
+    For example if a particular string used 200 out of 
+    the 256 possible unique bytes, there are only 56 
+    different bytes we can use in the compression.
+    
+    Parameters: 
+        
+        string: a string of text you want compressed.
+    
+    Result:
+    
+        a list of all the unique bytes 
+        not present in the string.
+    
+    '''
     used_bytes = set(string)
     return ALL_BYTES.difference(used_bytes)
 
+
 def get_ngrams(string, ngram_size):
+    '''
+    Breaks the string entered into chunks, formally called ngrams, 
+    with the size given by the parameter ngram_size.
+    These ngrams are then added to a list, result.
+    
+    Parameters:
+    
+        string: a string of text you want compressed.
+        ngram_size: the size of each ngram of the string. 
+                    For example if we broke the string 'apple' 
+                    with ngram size of 2 it would be 
+                    ['ap', 'pp', 'pl', 'le']
+    
+    Result:
+    
+        A list of all the ngrams of text in the string, 
+        with size in characters given by the ngram_size.
+
+    '''
     result = []
     upper_index = len(string) - ngram_size
     for index in range(upper_index + 1):
@@ -18,7 +74,25 @@ def get_ngrams(string, ngram_size):
         result.append(next_ngram)
     return result
 
+
 def freqs(seq):
+    '''
+    Counts the number of times each individual ngram appears in the list.
+    This data is mapped to a dictionary.
+    Each different ngram will have a number for each 
+    time it appears in the sequence
+    
+    Parameters:
+    
+        seq: A list of ngrams. 
+             An example input could be the get_ngram function result
+    
+    Result:
+    
+        A dictionary which lists all the different ngrams 
+        and the number of times they appeared in the given seq
+
+    '''
     counters = {}
     for item in seq:
         if item in counters:
@@ -27,10 +101,43 @@ def freqs(seq):
             counters[item] = 1
     return counters
 
+
 def get_second_item(seq):
+    '''
+    Returns the second item in the list
+    
+    Parameters:
+    
+        seq: A list of ngrams.
+    
+    Result:
+    
+       The second value in the list.
+
+    '''
     return seq[1]
 
+
 def sorted_ngrams_by_freq(dict):
+    '''
+    The ngams in the dictionary are sorted by the 
+    number associated with them, which represents 
+    the number of times they appear in the string.
+    This sorted frequency is put into a list called result
+    
+    Parameters:
+    
+        dict: A dictionary of ngrams with an associated number 
+              which respresents how many times they 
+              appeared in the original string.
+    
+    Result:
+    
+        A dictionary sorted by the number associated with each ngram.
+        The ngrams which appear most frequently 
+        will appear towards the start of the dictionary.
+
+    '''
     items = dict.items()
     sorted_items = sorted(items, key=get_second_item, reverse=True)
     result = []
@@ -38,9 +145,23 @@ def sorted_ngrams_by_freq(dict):
         result.append(ngram)
     return result
 
+
 MAX_MAPPINGS = 255
 
 def make_ngram_encoding(sorted_ngrams, encoding_bytes):
+    '''
+    For evry ngram in the list of sorted ngrams and encoding byte
+    Add the encoding byte to the list of result
+    If this list of encoding bytes becomes equal or greater than 255 stop the function
+
+    The result is returned at the end
+    
+    Parameters:
+    
+        sorted_ngrams: A list of the ngrams, sorted by their frequency in the original string.
+        encoding_bytes:
+
+    '''
     result = {}
     count = 0
     for ngram, encoding_byte in zip(sorted_ngrams, encoding_bytes):
@@ -50,7 +171,16 @@ def make_ngram_encoding(sorted_ngrams, encoding_bytes):
         count += 1
     return result
 
+
 def make_header(ngram_size, encoded_ngrams):
+    '''
+    
+    Parameters:
+    
+        ngram_size:
+        encoded_ngrams:
+    
+    '''
     number_mappings = len(encoded_ngrams)
     number_mappings_as_char = chr(number_mappings)
     ngram_size_as_char = chr(ngram_size)
@@ -59,7 +189,22 @@ def make_header(ngram_size, encoded_ngrams):
         result += ngram + encoded_ngrams[ngram]
     return result
 
+
 def make_encoded_string(string, ngram_size, encoded_ngrams):
+    '''
+    
+    
+    
+    Parameters:
+    
+        string:
+        ngram_size:
+        encoded_ngrams:
+    
+    Result
+    
+    
+    '''
     result  = ''
     index = 0
     while index < len(string):
@@ -72,9 +217,23 @@ def make_encoded_string(string, ngram_size, encoded_ngrams):
             index += 1
     return result
 
+
 MINIMUM_ENCODING_BYTES = 1
 
 def compress_file(ngram_size, in_filename, out_filename):
+    '''
+    
+    
+    Parameters:
+    
+        ngram_size:
+        in_filename:
+        out_filename:
+    
+    Result:
+    
+    
+    '''
     if ngram_size <= 0:
         print("n-gram size must be greater than 0")
         return
---
+++ decompress.py
@@ -0,0 +1,194 @@
+'''
+Decompression
+
+Author: Alexander Ryan
+
+This program takes a compressed file and decompresses the file back
+to its original state. The input is any compressed .txt file and the
+output is the .txt in its decompressed normal form.
+
+Compressed contents 
+
+
+Revision history:
+
+25 Sep 2014: Implemented get_header_info function.
+26 Sep 2014: Implemented parse_header function and 
+             get_compressed_body function.
+27 Sep 2014: Implemented decompress_body function.
+28 Sep 2014: Implemented decompress_file function.
+30 Sep 2014: Comments added.
+2  Oct 2014: Ran code through pylint, fixed all warnings.
+
+
+'''
+# Task 2: Get header information
+def get_header_info(compressed_contents):
+    '''
+    Retrieves the number of mappings and ngram size from a compressed text file. 
+    The number of mappings is the first value of the string compressed contents, 
+    so hence we take the slice from the string.
+        e.g. compressed_contents[0]
+    The ngram size is the second value, so we take the second slice.
+        e.g. compressed_contents[1]
+    We then use ord() to convert the Unicode object into an integer.
+    
+    Parameters:
+    
+        compressed_contents: A file that has been compressed 
+        with a particular ngram size, using the function 
+        compress_file in the compress.py program.
+    
+    Result:
+    
+        A two element tuple of integers. The first integer 
+        represents the number of mappings, the number of ngrams 
+        that have been swapped with an encoding byte from the 
+        original uncompressed text file.
+        The second integer represents the size of each 
+        ngram that is being represented by each encoding byte.
+    
+    '''
+    number_mappings = ord(compressed_contents[0])
+    ngram_size = ord(compressed_contents[1])
+    return (number_mappings, ngram_size)
+
+
+# Task 3: Parse the header
+def parse_header(compressed_contents):
+    '''
+    
+    
+    Parameters:
+    
+        compressed_contents: A file that has been compressed 
+        with a particular ngram size, using the function 
+        compress_file in the compress.py program.
+    
+    Result:
+    
+        A dictionary that represents the mapping in the header of the compressed contents.
+    
+    '''
+    number_mappings = get_header_info(compressed_contents)[0]
+    ngram_size = get_header_info(compressed_contents)[1]
+    
+    decode_map = {}    #the dictionary is called decode_map
+    index1 = 2
+    index2 = 2 + ngram_size
+    index3 = 2 + ngram_size
+    while index3 < (number_mappings * (ngram_size + 1) + ngram_size + 1):
+        decode_map[compressed_contents[index3]] = compressed_contents[index1:index2]
+        index1 += (ngram_size + 1)
+        index2 += (ngram_size + 1)
+        index3 += (ngram_size + 1)
+    return decode_map
+                    
+# Task 4: Get the compressed body
+def get_compressed_body(compressed_contents):
+    '''
+    
+    
+    Parameters:
+    
+        compressed_contents: A file that has been compressed 
+        with a particular ngram size, using the function 
+        compress_file in the compress.py program.
+    
+    
+    Result:
+    
+    
+    '''
+    number_mappings = get_header_info(compressed_contents)[0]
+    ngram_size = get_header_info(compressed_contents)[1]
+    
+    index = 2 + number_mappings * (ngram_size + 1)
+    compressed_body = compressed_contents[index:]
+    return compressed_body
+    
+
+# Task 5: Decompress the body
+def decompress_body(decode_map, compressed_body):
+    '''
+    
+    
+    Parameters:
+    
+        decode_map:
+        compressed_body:
+    if i.isalpha() == False: 
+            decompressed_contents += decode_map[i]
+        elif i == ' ':
+            decompressed_contents += i
+        else:
+            decompressed_contents += i
+    
+    i.isalpha() == True or i == '_' or i == ' ' or i == '.' or i == '"' or i == '0' or i == '1' or i == '2' or i == '3' or i == '4' or i == '5' or i == '6' or i == '7' or i == '8' or i == '9' or i == '\n' or i == ';' or i == "'" or i == '-' or i == '?' or i == '!' or i == ',' or i == '(' or i == ')' or i == ':':
+    
+     if ord(i) in range(32, 126) or i == '\n':
+            decompressed_contents += i
+        else:
+            decompressed_contents += decode_map[i]
+    '''
+    decompressed_contents = ''
+    for i in compressed_body:
+        if i in decode_map.keys():
+            decompressed_contents += decode_map[i]
+        else:
+            decompressed_contents += i
+    return decompressed_contents
+
+
+
+# Task 6: Tie it all together
+def decompress_file(in_filename, out_filename):
+    
+    in_file = open(in_filename)
+    compressed_contents = in_file.read()
+    in_file.close()
+    
+    decode_map = parse_header(compressed_contents)
+    compressed_body = get_compressed_body(compressed_contents)
+    uncompressed_contents = decompress_body(decode_map, compressed_body)
+    
+    out_file = open(out_filename, 'w')    
+    out_file.write(uncompressed_contents)
+    out_file.close()
+    #return compressed_contents
+    #return get_header_info(compressed_contents)
+    #return decode_map
+    #return compressed_body
+    #return uncompressed_contents
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
